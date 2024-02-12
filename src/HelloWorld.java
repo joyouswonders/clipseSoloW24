0e581fbe1216fec2c81095acf71002e72e6fcbb5
joyouswonders <jpoling1@emich.edu>
2024-02-12 12:07:29 -0500

    Make changes to the same line of HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from UserA");
-		System.out.println("Second print statement by alternate");
+		System.out.println("Second print statement by alternate, making changes to the same line of HelloWorld");
 
 	}
 
